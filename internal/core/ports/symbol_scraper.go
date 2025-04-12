077d4f7f38c2a148e183b24e4220c2c38d44e360
StevenSopilidis <sopilidimaria@gmail.com>
2025-04-12 13:49:40 +0300

    refactored interface

--- internal/core/ports/symbol_scraper.go
+++ internal/core/ports/symbol_scraper.go
@@ -1,7 +1,5 @@
 package ports
 
-import "github.com/stevensopi/smart_investor/symbol_scraper/internal/core/domain"
-
 type SymbolScraper interface {
-	Scrape(symbols []domain.Symbol) []domain.ScrapeResult
+	Scrape()
 }
